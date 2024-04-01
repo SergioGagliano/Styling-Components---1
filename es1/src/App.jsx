c39f6c43bfecf2cc64c9fe4cf45f735673495dab
Sergio Gagliano <sergiogagliano8@gmail.com>
2024-04-01 20:38:36 +0200

    esercizio completato

---
+++ es1/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { Index } from './components/Index.jsx'
+function App() {
+  return (
+    <>
+      <Index/>
+    </>
+  )
+}
+
+export default App
---
+++ es1/src/components/Welcome.jsx
@@ -0,0 +1,3 @@
+export function Welcome(){
+    return <button className="btn">Welcome</button>
+}
